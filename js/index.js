8f4aaf7d8038485e22b8d7893e93e6078f0155e6
cfraser321 <104275878+cfraser321@users.noreply.github.com>
2022-10-30 14:52:37 -0600

    Replaced old version

--- js/index.js
+++ js/index.js
@@ -1,14 +1,34 @@
 console.log("your index.js file is loaded correctly!");
 
 function scrollToAnchor(aid){
+  const destination = $("#about"+ aid);
+    $('html,body').animate({
+      scrollTop: destination.offset().top
+    },'slow');
+}
+
+$(document).on('click', '#smooth-link1', function(){
+  scrollToAnchor('#about');
+})
+
+function scrollToAnchor(aid){
     const destination = $("#skills"+ aid);
       $('html,body').animate({
         scrollTop: destination.offset().top
       },'slow');
   }
   
-  $(document).on('click', '.smooth-link', function(){
-    scrollToAnchor('.skills');
+  $(document).on('click', '#smooth-link2', function(){
+    scrollToAnchor('#skills');
   })
   
- 
+  function scrollToAnchor(aid){
+    const destination = $("#work"+ aid);
+      $('html,body').animate({
+        scrollTop: destination.offset().top
+      },'slow');
+  }
+  
+  $(document).on('click', '#smooth-link3', function(){
+    scrollToAnchor('#work');
+  })
